457009ede10502e722166eeeecce1f58d1e46d35
nasib15 <nasibhossain57@gmail.com>
2025-07-08 05:53:59 +0600

    Add: models and interfaces

---
+++ src/app/models/books.model.ts
@@ -0,0 +1,54 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/books.interfaces";
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      default: "FICTION",
+    },
+    isbn: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    copies: {
+      type: Number,
+      required: true,
+      min: [0, "The copies must be positive but got {VALUE}"],
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const Book = model("Book", bookSchema);
---
+++ src/app/models/borrow.model.ts
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+import { IBorrow } from "../interfaces/borrow.interfaces";
+
+const borrowSchema = new Schema<IBorrow>(
+  {
+    book: {
+      type: Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);

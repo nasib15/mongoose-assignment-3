044603981d1fddcbafd77dcaa7c214bd16fae3d2
nasib15 <nasibhossain57@gmail.com>
2025-07-08 14:23:54 +0600

    Add: Book routes controllers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import bookRoutes from "./app/controllers/book.controller";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use("/api", bookRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library Management API");
---
+++ src/app/controllers/book.controller.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from "express";
+import { Book } from "../models/books.model";
+
+const bookRoutes = express.Router();
+
+bookRoutes.post("/books", async (req: Request, res: Response) => {
+  const bookData = req.body;
+  const book = await Book.create(bookData);
+  res.status(201).json({
+    success: true,
+    message: "Book created successfully",
+    data: book,
+  });
+});
+
+bookRoutes.get("/books", async (req: Request, res: Response) => {
+  const books = await Book.find();
+
+  res.status(200).json({
+    success: true,
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
+
+bookRoutes.get("/books/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const book = await Book.findById(id);
+
+  res.status(200).json({
+    success: true,
+    message: "Book retrieved successfully",
+    data: book,
+  });
+});
+
+bookRoutes.put("/books/:bookId", async (req: Request, res: Response) => {
+  const updateBookData = req.body;
+  const { bookId } = req.params;
+
+  const response = await Book.findByIdAndUpdate(bookId, updateBookData);
+
+  res.status(201).json({
+    success: true,
+    message: "Book updated successfully",
+    data: response,
+  });
+});
+
+bookRoutes.delete("/books/:bookId", async (req: Request, res: Response) => {
+  const { bookId } = req.params;
+
+  const response = await Book.findByIdAndDelete(bookId);
+
+  res.status(204).json({
+    success: true,
+    message: "Book deleted successfully",
+    data: null,
+  });
+});
+
+export default bookRoutes;

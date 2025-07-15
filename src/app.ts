b48b418ba609eff472cf8ca9dd05a052bf91f411
nasib15 <nasibhossain57@gmail.com>
2025-07-15 12:31:40 +0600

    Add: 404 error handling middleware for invalid URL requests.

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,13 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library Management API");
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `The requested URL is not valid`,
+  });
+});
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({
     message: err.message || "Validation failed",
